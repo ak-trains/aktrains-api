eb9ee71900b1d3922876d95ac81c6fe15f497bb3
aktrains <140910068+aktrains@users.noreply.github.com>
2025-10-22 06:14:33 +0530

    Old History server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-import { APP_PORT } from "./config";
-import { errorHandler } from "./middlewares";
+import {APP_PORT} from "./config";
+import {errorHandler,apiHandler} from "./middlewares";
 import express from "express";
 import routes from "./routes";
 import { validator } from "./validators";
-import { CustomErrorService } from "./services";
-import rateLimit from "express-rate-limit";  // Note: Use default import for consistency
+import {CustomErrorService } from "./services";
+import { rateLimit } from "express-rate-limit";
 import { TOO_MANY_REQUESTS } from "./constants";
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended:false}));
 
 app.use(express.json());
 
-const rtLimit = rateLimit({windowMs: 1000,max: 1,handler:(req,res,next,opt)=>{
-    return next(CustomErrorService.tooManyRequests(TOO_MANY_REQUESTS));
-  }});
+const rtLimit = rateLimit({windowMs:1000,max:1,handler:(req,res,next,opt)=>{
+  return next(CustomErrorService.tooManyRequests(TOO_MANY_REQUESTS));
+}});
 
-app.use("/", rtLimit, validator.apiAuth(),apiHandler,routes);
+app.use("/",rtLimit,validator.apiAuth(),apiHandler,routes);
 
 app.use(errorHandler);
 
